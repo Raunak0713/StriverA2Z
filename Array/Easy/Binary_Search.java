e516421e72f162a28ad6760b483d2a765e0dc7b3
Raunak0713 <raunakbhanarkar7@gmail.com>
2024-07-11 14:11:18 +0530

    Binary Search

---
+++ Array/Easy/Binary_Search.java
@@ -0,0 +1,23 @@
+package Array.Easy;
+
+public class Binary_Search {
+    static int searchInSorted(int arr[], int N, int K) {
+        int low = 0;
+        int high = N - 1;
+        
+        while (low <= high) {
+            int mid = low + (high - low) / 2; 
+
+            if (arr[mid] == K) {
+                return 1;
+            } 
+            if (arr[mid] < K) {
+                low = mid + 1; 
+            } else {
+                high = mid - 1;
+            }
+        }
+        
+        return -1; 
+    }
+}
