1795430f09dd1c2a6bc87f5d163bb460bfd957fe
Raunak0713 <raunakbhanarkar7@gmail.com>
2024-07-14 18:20:08 +0530

    Minimum In Sorted Array

---
+++ Binary_Search/BS_on_1D_Array/Minimum_in_Sorted_Array.java
@@ -0,0 +1,30 @@
+package Binary_Search.BS_on_1D_Array;
+
+public class Minimum_in_Sorted_Array {
+    public int findMin(int[] nums) {
+        int low = 0;
+        int high = nums.length - 1;
+        int ans = Integer.MAX_VALUE;
+
+        while(low <= high){
+            int mid = low + (high-low)/2;
+
+            if(nums[low] == nums[mid] && nums[mid] == nums[high]){
+                low++;
+                high--;
+                continue;
+            }
+
+            if(nums[low] <= nums[mid]){
+                ans = Math.min(ans,nums[low]);
+                low = mid + 1;
+            }
+            else if(nums[mid] <= nums[high]){
+                ans = Math.min(ans, nums[mid]);
+                high = mid - 1;
+            }
+        }
+
+        return ans;
+    }
+}
