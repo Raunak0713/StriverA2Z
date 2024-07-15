829cbfda806cc44b65f1fc7ee1968de7630fa999
Raunak0713 <raunakbhanarkar7@gmail.com>
2024-07-15 19:49:05 +0530

    Level Order Traversal

---
+++ Binary_Tree/Traversal/LevelOrder.java
@@ -0,0 +1,33 @@
+package Binary_Tree.Traversal;
+
+import java.util.ArrayList;
+import java.util.LinkedList;
+import java.util.List;
+import java.util.Queue;
+
+public class LevelOrder {
+    public List<List<Integer>> levelOrder(TreeNode root) {
+        List<List<Integer>> ans = new ArrayList<>();
+        Queue<TreeNode> que = new LinkedList<>();
+        if(root == null){
+            return ans;
+        }
+
+        que.add(root);
+        while(!que.isEmpty()){
+            List<Integer> row = new ArrayList<>();
+            int level = que.size();
+            for(int i=0; i<level; i++){
+                if(que.peek().left != null){
+                    que.add(que.peek().left);
+                }
+                if(que.peek().right != null){
+                    que.add(que.peek().right);
+                }
+                row.add(que.poll().val);
+            }
+            ans.add(row);
+        }
+        return ans;
+    }
+}
