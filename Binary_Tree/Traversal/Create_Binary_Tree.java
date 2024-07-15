8569a8e148cb657582b7e9fb85aa42dff4edddda
Raunak0713 <raunakbhanarkar7@gmail.com>
2024-07-15 15:56:17 +0530

    Create Binary Tree

---
+++ Binary_Tree/Traversal/Create_Binary_Tree.java
@@ -0,0 +1,28 @@
+package Binary_Tree.Traversal;
+
+import java.util.ArrayList;
+
+class Node{
+    int val;
+    Node right;
+    Node left;
+
+    Node(int x){
+        x = val;
+    }
+}
+
+public class Create_Binary_Tree {
+    public static void createTree(Node root0, ArrayList<Integer> v ){
+        root0.val = v.get(0);
+        
+        root0.left = new Node(v.get(1));
+        root0.right = new Node(v.get(2));
+        
+        root0.left.left = new Node(v.get(3));
+        root0.left.right = new Node(v.get(4));
+        
+        root0.right.left = new Node(v.get(5));
+        root0.right.right = new Node(v.get(6));
+    }
+}
