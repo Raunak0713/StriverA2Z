4b38f67a1531ed3b643a6e2d325d251e9581ebb7
Raunak0713 <raunakbhanarkar7@gmail.com>
2024-07-16 01:07:21 +0530

    Iterative Pre Order Traversal

---
+++ Binary_Tree/Traversal/IterativePreOrder.java
@@ -0,0 +1,28 @@
+package Binary_Tree.Traversal;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Stack;
+
+public class IterativePreOrder {
+    public List<Integer> preorderTraversal(TreeNode root) {
+        List<Integer> ans = new ArrayList<>();
+        if(root == null){
+            return ans;
+        }
+        Stack<TreeNode> st = new Stack<>();
+        st.push(root);
+
+        while(!st.isEmpty()){
+            root = st.pop();
+            ans.add(root.val);
+            if(root.right != null){
+                st.push(root.right);
+            }
+            if(root.left != null){
+                st.push(root.left);
+            }
+        }
+        return ans;
+    }
+}
