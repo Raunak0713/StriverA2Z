3abdc9d413c058907d47eda42810d34c87596f60
Raunak0713 <raunakbhanarkar7@gmail.com>
2024-07-17 12:59:45 +0530

    ZigZag Level Order Traversal

---
+++ Binary_Tree/Medium_Problems/ZigZag_Level_Traversal.java
@@ -0,0 +1,40 @@
+package Binary_Tree.Medium_Problems;
+
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.LinkedList;
+import java.util.List;
+import java.util.Queue;
+
+public class ZigZag_Level_Traversal {
+    public List<List<Integer>> zigzagLevelOrder(TreeNode root) {
+        List<List<Integer>> ans = new ArrayList<>();
+        if (root == null) return ans;
+
+        Queue<TreeNode> queue = new LinkedList<>();
+        queue.add(root);
+        boolean leftToRight = true;
+
+        while (!queue.isEmpty()) {
+            int size = queue.size();
+            List<Integer> row = new ArrayList<>();
+            for (int i = 0; i < size; i++) {
+                TreeNode node = queue.poll();
+                row.add(node.val);
+                if (node.left != null) {
+                    queue.add(node.left);
+                }
+                if (node.right != null) {
+                    queue.add(node.right);
+                }
+            }
+            if (!leftToRight) {
+                Collections.reverse(row);
+            }
+            ans.add(row);
+            leftToRight = !leftToRight;
+        }
+
+        return ans;
+    }
+}
