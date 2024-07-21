9934d578de92123566218ed32fddc40a08eae7cf
Raunak0713 <raunakbhanarkar7@gmail.com>
2024-07-21 10:52:49 +0530

    Implementation of Linked List

---
+++ LinkedList/Singly_Linked_List/Implementation.java
@@ -0,0 +1,9 @@
+package LinkedList.Singly_Linked_List;
+
+public class Implementation {
+    public static void main(String[] args) {
+        int[] arr = {1, 5, 2, 7};
+        Node y = new Node(arr[2]);
+        System.out.println(y.data);
+    }
+}
