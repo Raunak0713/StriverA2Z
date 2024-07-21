c263e3c56521bab897521a701ba44e81b1446d7a
Raunak0713 <raunakbhanarkar7@gmail.com>
2024-07-21 10:50:33 +0530

    Linked List Node Implementation

---
+++ LinkedList/Singly_Linked_List/Node.java
@@ -0,0 +1,16 @@
+package LinkedList.Singly_Linked_List;
+
+public class Node {
+    public int data;
+    public Node next;
+
+    Node(int data, Node next){
+        this.data = data;
+        this.next = next;
+    }
+
+    Node(int data){
+        this.data = data;
+        this.next = null;
+    }
+}
