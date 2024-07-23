17fe575c89ccb0519ce2efc79b823393c23efc0b
Raunak0713 <raunakbhanarkar7@gmail.com>
2024-07-23 16:29:01 +0530

    Fibonacci Memoization

---
+++ Dynamic_Programming/One_Dimensional/Fibonacchi/Fibo_Memoization.java
@@ -0,0 +1,21 @@
+package Dynamic_Programming.One_Dimensional.Fibonacchi;
+
+import java.util.Arrays;
+
+public class Fibo_Memoization {
+    class Solution {
+    int[] DP;
+    public int fib(int n) {
+        DP = new int[n+1];
+        Arrays.fill(DP, -1);
+        return solve(n);
+    }
+    public int solve(int n){
+        if(n<=1) DP[n] = n;
+        if(DP[n] != -1){
+            return DP[n];
+        }
+        return DP[n] = solve(n-1) + solve(n-2);
+    }
+}
+}
