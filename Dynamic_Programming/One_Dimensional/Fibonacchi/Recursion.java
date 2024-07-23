aca338c399789c0351a1c3f430da79b17c494a57
Raunak0713 <raunakbhanarkar7@gmail.com>
2024-07-23 16:23:49 +0530

    Fibonacci Recursion

---
+++ Dynamic_Programming/One_Dimensional/Fibonacchi/Recursion.java
@@ -0,0 +1,11 @@
+package Dynamic_Programming.One_Dimensional.Fibonacchi;
+
+public class Recursion {
+    public int fib(int n) {
+        return solve(n);
+    }
+    public int solve(int n){
+        if(n<=1) return n;
+        return solve(n-1) + solve(n-2);
+    }
+}
